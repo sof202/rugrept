35906c313d57376460ab7429c566920bb1722776
sof202 <s.o.fletcher@exeter.ac.uk>
2025-08-21 22:19:54 +0100

    feat: send error messages to standard error stream
    This is opposed to the standard output stream that `println!` sends to.

--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,11 @@ fn main() {
     let command_line_arguments: Vec<String> = env::args().collect();
     let config =
         Config::build(&command_line_arguments).unwrap_or_else(|err| {
-            println!("Problem passing arguments: {err}");
+            eprintln!("Problem passing arguments: {err}");
             process::exit(2);
         });
     if let Err(error) = run(config) {
-        println!("[rugrept] Error: {error}");
+        eprintln!("[rugrept] Error: {error}");
         process::exit(1);
     }
 }
